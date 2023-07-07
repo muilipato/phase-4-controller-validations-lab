b8b8430defe4f2215a4913f8f7a2f92f568f87d3
Patrick <muilipato@gmail.com>
2023-07-07 18:08:57 +0300

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,17 @@
 class AuthorsController < ApplicationController
-  
-  def show
-    author = Author.find(params[:id])
-
-    render json: author
-  end
-
   def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
+    author = Author.new(author_params)
+    if author.save
+      render json: author, status: :created
+    else
+      render json: { errors: author.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
-  
+
   def author_params
-    params.permit(:email, :name)
+    params.require(:author).permit(:name, :email)
   end
-  
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,18 @@
 class PostsController < ApplicationController
 
-  def show
-    post = Post.find(params[:id])
-    
-    render json: post
-  end
-
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
+    if post.update(post_params)
+      render json: post
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
 
   def post_params
-    params.permit(:category, :content, :title)
+    params.require(:post).permit(:title, :category, :content)
   end
 
 end
